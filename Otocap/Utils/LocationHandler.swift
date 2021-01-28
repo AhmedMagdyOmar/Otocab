5caea61ec7f139750fe5704e6321b76c053f145b
AhmedMagdyOmar <ahmedomar042@gmail.com>
2021-01-28 02:55:07 +0200

    First Commit

---
+++ Otocap/Utils/LocationHandler.swift
@@ -0,0 +1,29 @@
+//
+//  LocationHandler.swift
+//  Otocab
+//
+//  Created by Ahmed Omar on 6/13/20.
+//  Copyright © 2019 Khyer. All rights reserved.
+//
+
+import CoreLocation
+
+class LocationHandler: NSObject, CLLocationManagerDelegate {
+    
+    static let shared = LocationHandler()
+    var locationManager: CLLocationManager!
+    var location: CLLocation?
+    
+    override init() {
+        super.init()
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.requestAlwaysAuthorization()
+        }
+    }
+}
